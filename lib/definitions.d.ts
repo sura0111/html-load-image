aa9ae82796f9159b22ed6c18c369efcfd5937085
sura.kh <sura.kh@rakuten.com>
2022-11-09 10:19:59 +0900

    v1.3.7

--- lib/definitions.d.ts
+++ lib/definitions.d.ts
@@ -1,5 +1,5 @@
 import { LoadImageOptions as BlueImpLoadImageOptions } from 'blueimp-load-image';
 export declare type LoadImageOptions = Omit<BlueImpLoadImageOptions, 'orientation' | 'meta' | 'canvas'>;
-export declare type LoadImagesOptions = LoadImageOptions & {
+export interface LoadImagesOptions extends LoadImageOptions {
     maxLoadImageCount: number;
-};
+}
